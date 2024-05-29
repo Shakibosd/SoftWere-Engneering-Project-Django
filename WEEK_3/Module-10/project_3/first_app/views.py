7753fb5dc0176048e56196cd7c5f24fbe982362f
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-05-29 18:24:33 +0600

    finish

---
+++ WEEK_3/Module-10/project_3/first_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    d = {'author' : 'rohim', 'age' : '20', 'lst' : [1,2,3]}
+    return render(request, 'home.html', d)
