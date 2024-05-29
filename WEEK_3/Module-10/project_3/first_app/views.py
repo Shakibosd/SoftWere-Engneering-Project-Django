b6bac9c91cebbae1cbe3700a86a8ba2a3fa2b684
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-05-29 23:52:06 +0600

    finish

--- WEEK_3/Module-10/project_3/first_app/views.py
+++ WEEK_3/Module-10/project_3/first_app/views.py
@@ -1,6 +1,25 @@
+
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(request):
-    d = {'author' : 'rohim', 'age' : '15', 'lst' : [1,2,3]}
+    d = {'author' : 'rohim', 'age' : 15, 'lst' : ['python','is','best'], 'birthday' : datetime.datetime.now(), 'val' : '', 'courses' : [
+
+        {
+            'id' : 1,
+            'name' : 'Python',
+            'fee' : 5000
+        },
+        {
+            'id' : 2,
+            'name' : 'django',
+            'fee' : 10000
+        },
+        {
+            'id' : 3,
+            'name' : 'c',
+            'fee' : 1000
+        },
+    ]}
     return render(request, 'home.html', d)
