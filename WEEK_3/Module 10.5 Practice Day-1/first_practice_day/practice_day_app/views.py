8cc7f8151f201280e7128c681249de62563394a2
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-05-31 21:56:32 +0600

    finish

--- WEEK_3/Module 10.5 Practice Day-1/first_practice_day/practice_day_app/views.py
+++ WEEK_3/Module 10.5 Practice Day-1/first_practice_day/practice_day_app/views.py
@@ -4,23 +4,31 @@ import datetime
 # Create your views here.
 
 def home(request):
-
-    x = {'author' : 'shakib', 'age' : 5, 'lst' : ['shakib', 'is', 'bed'], 'birthday' : datetime.datetime.now(),
+    x = {'author' : 'shakib', 'age' : 5, 'lst' : ['Shakib', 'Is', 'Bed', 'But', 'She Tring', 'Good'], 'birthday' : datetime.datetime.now(), 'roll' : 10, 'title' : 'syed', 'val' : 'hi', 'lists' : [10,20,30,40,50,60,70,80,90,100],
     'cources' : [
           {
               'id' : 1,
               'name' : 'shakib',
-              'fee' : 5000
+              'fee' : 5000,
+              'reg' : 12234244    
           },
           {
               'id' : 2,
               'name' : 'rakib',
-              'fee' : 10040
+              'fee' : 10040,
+              'reg' : 12234245
           },
           {
               'id' : 3,
               'name' : 'nokib',
-              'fee' : 10050
+              'fee' : 10050,
+              'reg' : 12234246
+          },
+          {
+              'id' : 4,
+              'name' : 'zillur',
+              'fee' : 10090,
+              'reg' : 12234247
           },
     ]}
          
