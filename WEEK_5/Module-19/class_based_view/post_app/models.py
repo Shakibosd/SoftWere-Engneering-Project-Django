540da149640926176881e683f1ca8d5f1fedc0e1
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-06-18 02:47:23 +0600

    finsh

--- WEEK_5/Module-19/class_based_view/post_app/models.py
+++ WEEK_5/Module-19/class_based_view/post_app/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.db import models 
 from categories_app.models import Category
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth.models import User 
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
@@ -10,4 +10,14 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_app/media/uploads/', blank=True, null=True)
 
     def __str__(self):
-        return self.title  
+        return self.title   
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')    
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)#jhokon e ei class er object toiri hobe sei time ta rekhe dibo
+
+    def __str__(self):
+        return f"Comments by {self.name}"
--- WEEK_5/Module-19/class_based_view/post_app/views.py
+++ WEEK_5/Module-19/class_based_view/post_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect 
+from django.urls import reverse_lazy 
 from . import forms
 from . import models
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required 
-from django.views.generic import CreateView, UpdateView, DeleteView, DetailView 
+from django.utils.decorators import method_decorator 
+from django.contrib.auth.decorators import login_required  
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView  
 
 
 
@@ -71,5 +71,26 @@ class deletePostView(DeleteView):
 
 class DetailPostView(DetailView):
     model = models.Post
-    pk_url_kwarg = 'id'
+    pk_url_kwarg = 'id' 
     template_name = './post_app/post_details.html'
+
+    def post(self, request, *args, **kwargs):
+        comment_form = forms.CommentForm(data=self.request.POST)
+        post = self.get_object()
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        return self.get(request, *args, **kwargs)    
+
+  
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object #post model er object ekhane store korlam
+        comments = post.comments.all()
+        comment_form = forms.CommentForm()
+
+        context['comments'] = comments
+        context['comment_form'] = comment_form
+        return context               
+        
