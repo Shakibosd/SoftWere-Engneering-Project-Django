ce1408fda31fe7091d064385f72a88128c69151a
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-06-22 01:35:27 +0600

    finish

--- WEEK_5/Mid-Tram Exam/author_app/forms.py
+++ WEEK_5/Mid-Tram Exam/author_app/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
 
     class Meta:
         model = User
