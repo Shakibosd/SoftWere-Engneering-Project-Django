f75283fa0dbecbb9d49a91f963730732177cca94
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-06-26 11:11:13 +0600

    finish

--- Week_6/Module-21/goriber_bank/accounts_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import userBankAccount, userAddress
-
-admin.site.register(userBankAccount)
-admin.site.register(userAddress)
--- Week_6/Module-21/goriber_bank/accounts_app/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path, include
-from .views import userRagisterView
-
-urlpatterns = [
-    path('register/', userRagisterView.as_view(), name='register'),
-]
