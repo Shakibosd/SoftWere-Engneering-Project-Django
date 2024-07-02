4d9edd585d7558d14c77fecb018729fbfac11b19
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-07-02 07:06:52 +0600

    finish

--- Week_6/Module-21,22,23 Bank Management/mamar_bank/accounts/views.py
+++ Week_6/Module-21,22,23 Bank Management/mamar_bank/accounts/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import FormView
+
+from transactions.views import send_transaction_email
 from .forms import UserRegistrationForm,UserUpdateForm
 from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
 from django.shortcuts import redirect
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
@@ -43,8 +50,26 @@ class UserBankAccountUpdateView(View):
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('profile')  # Redirect to the user's profile page
+            return redirect('profile') 
         return render(request, self.template_name, {'form': form})
     
     
-    
+
+#password change
+class PassChangeView(LoginRequiredMixin, FormView):
+    form_class = PasswordChangeForm
+    template_name = './accounts/pass_change.html'
+    success_url = reverse_lazy('profile')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        update_session_auth_hash(self.request, form.user)
+        messages.success(self.request, 'Password Updated Successfully')
+        send_transaction_email(self.request.user, 'amount', 'Password Change Notification', './accounts/password_change_email.html')
+        return super().form_valid(form)
+ 
--- Week_6/Module-21,22,23 Bank Management/mamar_bank/transactions/views.py
+++ Week_6/Module-21,22,23 Bank Management/mamar_bank/transactions/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.utils import timezone
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic import CreateView, ListView
-from transactions.constants import DEPOSIT, WITHDRAWAL,LOAN, LOAN_PAID
+from accounts.models import UserBankAccount
+from transactions.constants import DEPOSIT, SEND_MONEY, WITHDRAWAL,LOAN, LOAN_PAID
 from datetime import datetime
 from django.db.models import Sum
 from django.urls import reverse
@@ -15,6 +15,7 @@ from django.core.mail import EmailMessage, EmailMultiAlternatives
 
 from transactions.forms import (
     DepositForm,
+    SendMoneyForm,
     WithdrawForm,
     LoanRequestForm,
 )
@@ -77,32 +78,34 @@ class DepositMoneyView(TransactionCreateMixin):
         )
         send_transaction_email(self.request.user, amount, 'Deposit Message', 'transactions/deposite_email.html')
         return super().form_valid(form)
-
+    
 
 class WithdrawMoneyView(TransactionCreateMixin):
     form_class = WithdrawForm
-    title = 'Withdraw Money'
+    title = "Withdraw Money"
 
     def get_initial(self):
-        initial = {'transaction_type': WITHDRAWAL}
+        initial = {"transaction_type": WITHDRAWAL}
         return initial
 
     def form_valid(self, form):
-        amount = form.cleaned_data.get('amount')
+        amount = form.cleaned_data.get("amount")
+        bank_balance = UserBankAccount.objects.aggregate(total_balance=Sum("balance"))[
+            "total_balance"
+        ]
+        if bank_balance < amount:
+            messages.warning(self.request, "Bank Is Bnkrupt")
+        else:
+            self.request.user.account.balance -= form.cleaned_data.get("amount")
+            # balance = 300
+            # amount = 5000
+            self.request.user.account.save(update_fields=["balance"])
 
-        self.request.user.account.balance -= form.cleaned_data.get('amount')
-        # balance = 300
-        # amount = 5000
-        self.request.user.account.save(update_fields=['balance'])
-
-        messages.success(
-            self.request,
-            f'Successfully withdrawn {"{:,.2f}".format(float(amount))}$ from your account'
-        )
-
-        send_transaction_email(self.request.user, amount, 'Withdraw Message', 'transactions/withdraw_email.html')
-
+            messages.success(self.request,f'Successfully withdrawn {"{:,.2f}".format(float(amount))}$ from your account')
+            send_transaction_email(self.request.user, amount, 'Withdrawal Message', './transactions/withdraw_email.html')
         return super().form_valid(form)
+    
+    
 
 class LoanRequestView(TransactionCreateMixin):
     form_class = LoanRequestForm
@@ -196,3 +199,32 @@ class LoanListView(LoginRequiredMixin,ListView):
         return queryset
     
 
+class SendMoneyView(TransactionCreateMixin):
+    form_class = SendMoneyForm
+    template_name = "./transactions/sendmoney.html"
+    title = "Send Money"
+    success_url = reverse_lazy("transaction_report")
+
+    def get_initial(self):
+        initial = {"transaction_type": SEND_MONEY}
+        return initial
+
+    def form_valid(self, form):
+        account_no = form.cleaned_data.get("account_no")
+        amount = form.cleaned_data.get("amount")
+        sender = self.request.user.account
+
+        try:
+            reciver = UserBankAccount.objects.get(account_no=account_no)
+            reciver.balance += amount
+            sender.balance -= amount
+            reciver.save(update_fields=["balance"])
+            sender.save(update_fields=["balance"])
+            messages.success(self.request, "Send Money Successful")
+            send_transaction_email(self.request.user, amount, 'Send Money', 'transactions/sendmoney_email.html')
+            send_transaction_email(reciver.user, amount, 'Receive Money', 'transactions/receivemoney_email.html')
+
+            return super().form_valid(form)
+        except UserBankAccount.DoesNotExist:
+            form.add_error("account_no", "Invalid Account No")
+            return super().form_invalid(form)
